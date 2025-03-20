de0ac39913b12cb60cfa8b2315c8b74cb07a355b
Husain-Bohra <husain.23cse406@rtu.ac.in>
2025-03-21 02:20:15 +0530

    Add terser and vercel config

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-    },
-  },
-  server: {
-    open: true,
-    port: 3000,
-  },
   build: {
-    outDir: 'dist',
-    minify: 'terser',
-    sourcemap: false,
-  },
+    minify: 'terser', // Explicitly set terser as the minifier
+    terserOptions: {
+      compress: {
+        // Add compress options here if needed
+      }
+    }
+  }
 }) 
